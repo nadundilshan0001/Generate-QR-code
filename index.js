3377613f05fecac444de7423b484e3b357f56cf6
nadundilshan0001 <nadundilshanuni01@gmail.com>
2024-01-15 16:06:30 +0530

    Install body-parser

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-    res.send("Hello");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen("3000", function(){
