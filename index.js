c79ab5ed9a163e00871613e9ca65f4e1794791df
nadundilshan0001 <nadundilshanuni01@gmail.com>
2024-01-15 16:14:46 +0530

    Install qr-image

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+
+app.post("/", function(req, res){
+    const URL = req.body.url;
+    console.log(URL);
+})
+
 app.listen("3000", function(){
     console.log("Server is running on port 3000");
 });
